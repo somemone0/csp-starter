f4681566cc85a5bbdeabd3cbae6a6d9dd5b54e6a
TheCoder11 <jtsjohn05@gmail.com>
2023-05-23 22:27:54 -0700

    fixed url_for problem

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, session
-from flask.ext.session import Session
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 import routes
 
--- routes.py
+++ routes.py
@@ -1,11 +1,19 @@
 from __main__ import app
+from flask import render_template, session
+import requests
 
 # The default functions contained in this file match with the base.html values
 
 # Don't change the function names
 
+hostname = "http://5.161.86.174"
+
 @app.route("/") # Main page, containing list of users
 def main():
+    pass
+
+@app.route("/user/<username>") # User page, containing list of posts
+def user(username):
     pass
 
 @app.route("/login") # Login page, handles both get and post
@@ -24,3 +32,5 @@ def create_post():
 def logout():
     pass
 
+def is_logged_in():
+    return "token" in session
